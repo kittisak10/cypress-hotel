5f57a1cbda6633445cd55d534d69e5666ab1394e
Thaksin <thakstho@METROSYSTEMS.CO.TH>
2023-10-18 12:01:40 +0700

    [canfeepolicies] cypress

--- cypress/e2e/cancellation-fee-policy.cy.js
+++ cypress/e2e/cancellation-fee-policy.cy.js
@@ -1,32 +1,46 @@
 describe('template spec', () => {
+  // const currentTimestamp = Cypress.moment().format('YYYY-MM-DD HH:mm:ss');
 
-  before(() => {
-    cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTc0NTk4NjMsImV4cCI6MTY5NzU0NjI2M30.Y3BYP-H0WgF5o56aM6uAjL7XLbdhzM89PJFDeOPzdsg');
-    cy.viewport(1600, 1000)
-  });
+    before(() => {
+      cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTc2MDMwMjYsImV4cCI6MTY5NzY4OTQyNn0.y-tq8Cu6gJ8COYFNfjMVRN5Tt4HVdYBnPtfguiYBY_g');
+    });
+  
+    
+    it('create new', () => {  
+      cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/new')
+      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').type('YourValueHere');
+      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').should('have.value', 'YourValueHere');
+  
+      cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').type('YourValueHere');
+      cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').should('have.value', 'YourValueHere');
 
-  it('create new', () => {
-    cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/new')
+     
+      cy.get('#ms_cancellation_fee_policy_policy_currency').click();
+      cy.get('.ant-select-item-option[title="USD - Dollar"]').click();
+      cy.get('#rc-tabs-0-panel-0 > div > div.ant-col.ant-col-8.css-htwhyh > div > div > div > div > div.ant-collapse-content.ant-collapse-content-active > div > div > div.ant-col.ant-col-8.gutter-row.css-htwhyh > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div > div > span.ant-select-selection-item').should('have.text', 'USD - Dollar');
+      
+      cy.get('#ms_cancellation_fee_policy_cancelfee_trans_code').click();
+      cy.get('.ant-select-item-option[title="Description"]').click();
+      cy.get('#rc-tabs-0-panel-0 > div > div.ant-col.ant-col-8.css-htwhyh > div > div > div > div > div.ant-collapse-content.ant-collapse-content-active > div > div > div.ant-col.ant-col-8.gutter-row.css-htwhyh > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div > div > span.ant-select-selection-item').should('have.text', 'USD - Dollar');
+      
+      cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_to').type(10,{ force: true });
+      
+      cy.get('#ms_cancellation_fee_policy_calculation_mapping_loyalty_member').click();
+      cy.get('body > div:nth-child(6) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-now > a').click();
 
-    // policy name
-    cy.get('input[id="ms_cancellation_fee_policy_policy_name"]').type('YourValueHere');
-    cy.get('input[id="ms_cancellation_fee_policy_policy_name"]').should('have.value', 'YourValueHere');
+      cy.get('#ms_cancellation_fee_policy_calculation_mapping_guest').type('12:00:00', { force: true });
+      cy.get('body > div:nth-child(7) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-ok > button').click();
 
-    // currency
-    // Select use click to select dropdown
-    cy.get('#ms_cancellation_fee_policy_policy_currency').click();
-    cy.get('.ant-select-item-option[title="USD - Dollar"]').click();
-    cy.get('#rc-tabs-0-panel-0 > div > div.ant-col.ant-col-8.css-htwhyh > div > div > div > div > div.ant-collapse-content.ant-collapse-content-active > div > div > div.ant-col.ant-col-8.gutter-row.css-htwhyh > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div > div > span.ant-select-selection-item').should('have.text', 'USD - Dollar');
-
-    // internal loyalty program member
-    cy.get('#ms_cancellation_fee_policy_calculation_mapping_loyalty_member').type('12:00:00', { force: true });
-    cy.get('body > div:nth-child(5) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-ok > button').click();
-
-    cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_val').type(10);
-
-
-    // submit button
-    // cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
-    // cy.contains('Record(s) have been successfully saved.');
+      
+      cy.get('#ms_cancellation_fee_policy_calculation_mapping_time_unit').click();
+      cy.get('.ant-select-item-option[title="Days"]').click();
+    
+      cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_method').click();
+      cy.get('.ant-select-item-option[title="Percent of Total Stay"]').click();
+      
+      cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_val').type(10);
+  
+      cy.get('form.ant-form.ant-form-vertical.css-htwhyh').submit();
+      cy.contains('Record(s) have been successfully saved.');
+    })
   })
-})
