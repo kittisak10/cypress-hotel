a27bf3c695cf85f68b476407a4c4b42e9c528f99
Thaksin <thakstho@METROSYSTEMS.CO.TH>
2023-10-24 16:34:33 +0700

    edit canfee

--- cypress/e2e/cancellation-fee-policy/cancellation-fee-policy-edit.cy.js
+++ cypress/e2e/cancellation-fee-policy/cancellation-fee-policy-edit.cy.js
@@ -1,42 +1,44 @@
 describe('template spec', () => {
-    // const currentTimestamp = Cypress.moment().format('YYYY-MM-DD HH:mm:ss');
-  
-      before(() => {
-        cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIzOTQ2MmVlOC1kOWM1LTQzNzAtOGIxMi01OWJmOWI0YjQ3NzAiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gUG9ydGFsIiwiaWFkbSI6dHJ1ZSwiaWF0IjoxNjk4MTI2NjU2LCJleHAiOjE2OTgyMTMwNTZ9.UuvFvriNG2Py5TYzQHZ976vkqy_eDukNEalIRBFAGC0');
-      });
-    
-      it('edit', () => {  
-        cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/766655b7-5a13-4122-b29b-c258dada8c1f')
-        
-       
-  
-        // cy.get('.ant-btn css-htwhyh ant-btn-text ant-btn-sm ant-btn-icon-only ant-dropdown-trigger').click();
-        
-
-        // cy.get('.ant-dropdown-menu-title-content').click();
-        // cy.get('body > div:nth-child(7) > div > div > ul > li:nth-child(1) > span').click(); 
-
-      //   cy.get('#ms_cancellation_fee_policy_calculation_mapping_loyalty_member').click();
-      //   cy.get('body > div:nth-child(6) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-now > a').click();
-
-      //   cy.get('#ms_cancellation_fee_policy_calculation_mapping_guest').type('12:00:00', { force: true });
-      //   cy.get('body > div:nth-child(7) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-ok > button').click();
-
-      //   cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_to').type(10,{ force: true });
-
-      //   cy.get('#ms_cancellation_fee_policy_calculation_mapping_time_unit').click();
-      //   cy.get('.ant-select-item-option[title="Days"]').click();
-
-      //   cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_method').click();
-      //   cy.get('.ant-select-item-option[title="Percent of Total Stay"]').click();
-      //  cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_val').type(10);
-
-        
+  // const currentTimestamp = Cypress.moment().format('YYYY-MM-DD HH:mm:ss');
+  before(() => {
+  cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTgxMzI2MjAsImV4cCI6MTY5ODIxOTAyMH0.uOJeym5NNWhVEjAiZB4ALiSW4v_QO4Mrqkqm8ShU32w');
+  });
+    it('edit', () => {  
+      cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/766655b7-5a13-4122-b29b-c258dada8c1f')
+      
      
-        // cy.get('form.ant-form.ant-form-vertical.css-htwhyh').submit();
-        // cy.contains('Record(s) have been successfully saved.');
+  
+      cy.get('#rc-tabs-0-panel-0 > div > div.ant-col.ant-col-16.css-htwhyh > div > div > div > div > div.ant-collapse-content.ant-collapse-content-active > div > div > div > div > div.tw-overflow-x-auto.tw-flex.tw-gap-2.tw-snap-x.tw-scroll-smooth > div.tw-w-60.tw-h-24.tw-rounded-lg.tw-cursor-pointer.tw-p-2.tw-flex.tw-items-start.tw-snap-start > button > span > svg').click();
+      cy.get("body > div:nth-child(4) > div > div > ul > li:nth-child(1)").click();
+      // cy.get('.ant-dropdown-menu-title-content').click();
+      // cy.get('body > div:nth-child(7) > div > div > ul > li:nth-child(1) > span').click(); 
+  
+      // cy.get('#ms_cancellation_fee_policy_calculation_mapping_loyalty_member').click();
+      // cy.get('body > div:nth-child(7) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > form > div > div:nth-child(1) > div.ant-collapse-content.ant-collapse-content-active > div > div > div:nth-child(1) > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div').click();
+      cy.get( "body > div:nth-child(5) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > form > div > div:nth-child(1) > div.ant-collapse-content.ant-collapse-content-active > div > div > div:nth-child(1) > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div").click();
+      cy.get('body > div:nth-child(6) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-now > a').click();
+
+
+      cy.get("body > div:nth-child(5) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > form > div > div:nth-child(1) > div.ant-collapse-content.ant-collapse-content-active > div > div > div:nth-child(2) > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div").click();
+      // cy.get('body > div:nth-child(7) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > form > div > div:nth-child(1) > div.ant-collapse-content.ant-collapse-content-active > div > div > div:nth-child(2) > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div').type('12:00:00', { force: true });
+      cy.get('body > div:nth-child(7) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-ok > button').click();
+  
+      // cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_to').type(8,{ force: true });
+      cy.get('input[name="ms_cancellation_fee_policy_calculation_mapping_rule_to"]').type('8');
+  
+      cy.get('#ms_cancellation_fee_policy_calculation_mapping_time_unit').click({force: true});
+      cy.get('.ant-select-item-option[title="Days"]').click();
+  
+      cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_method').click();
+      cy.get('.ant-select-item-option[title="Percent of Total Stay"]').click();
+     cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_val').type(10);
+  
+      
+   
+      // cy.get('form.ant-form.ant-form-vertical.css-htwhyh').submit();
+      // cy.contains('Record(s) have been successfully saved.');
   
   
   
-      })
     })
+  })
--- cypress/e2e/cancellation-fee-policy/cancellation-fee-policy.cy.js
+++ cypress/e2e/cancellation-fee-policy/cancellation-fee-policy.cy.js
@@ -2,7 +2,7 @@ describe('template spec', () => {
   // const currentTimestamp = Cypress.moment().format('YYYY-MM-DD HH:mm:ss');
 
     before(() => {
-      cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTc3ODY0ODYsImV4cCI6MTY5Nzg3Mjg4Nn0.bbATzqjj2uzpfQAfCo-M1Xb9DInxcEL-Rr1FuZ0CNgA');
+      cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTgxMzU1NzUsImV4cCI6MTY5ODIyMTk3NX0.ft0yZdSjUjXfjIENLUUu87BH2taM-d6R45Wd8VAmNFw');
     });
   
     
@@ -37,49 +37,10 @@ describe('template spec', () => {
     
       cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_method').click();
       cy.get('.ant-select-item-option[title="Percent of Total Stay"]').click();
+      
       cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_val').type(10);
-
-      // cy.get('#rc-tabs-1-panel-0 > div > div.ant-col.ant-col-16.css-htwhyh > div > div > div > div > div.ant-collapse-content.ant-collapse-content-active > div > div > div > div > div.tw-overflow-x-auto.tw-flex.tw-gap-2.tw-snap-x.tw-scroll-smooth > div.tw-w-60.tw-h-24.tw-rounded-lg.tw-cursor-pointer.tw-p-2.tw-flex.tw-items-start.tw-snap-start > button').click();
-      // cy.get('body > div:nth-child(7) > div > div > ul > li:nth-child(1) > span').click();
   
       cy.get('form.ant-form.ant-form-vertical.css-htwhyh').submit();
       cy.contains('Record(s) have been successfully saved.');
-
-
-
-    })
-    before(() => {
-      cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTc3ODY0ODYsImV4cCI6MTY5Nzg3Mjg4Nn0.bbATzqjj2uzpfQAfCo-M1Xb9DInxcEL-Rr1FuZ0CNgA');
-    });
-    it('edit', () => {  
-      cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/766655b7-5a13-4122-b29b-c258dada8c1f')
-      
-     
-
-      cy.get('ant-btn css-htwhyh ant-btn-text ant-btn-sm ant-btn-icon-only ant-dropdown-trigger').click();
-
-
-      // cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_to').type(10,{ force: true });
-      
-      // cy.get('#ms_cancellation_fee_policy_calculation_mapping_loyalty_member').click();
-      // cy.get('body > div:nth-child(6) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-now > a').click();
-
-      // cy.get('#ms_cancellation_fee_policy_calculation_mapping_guest').type('12:00:00', { force: true });
-      // cy.get('body > div:nth-child(7) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-ok > button').click();
-
-      
-      // cy.get('#ms_cancellation_fee_policy_calculation_mapping_time_unit').click();
-      // cy.get('.ant-select-item-option[title="Days"]').click();
-    
-      // cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_method').click();
-      // cy.get('.ant-select-item-option[title="Percent of Total Stay"]').click();
-      // cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_val').type(10);
-
-   
-      cy.get('form.ant-form.ant-form-vertical.css-htwhyh').submit();
-      cy.contains('Record(s) have been successfully saved.');
-
-
-
     })
   })
