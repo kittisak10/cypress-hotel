964e1080db02298427f5eb5d42d6f9548f5b3da4
kittisak <kittiyim@METROSYSTEMS.CO.TH>
2023-10-24 17:52:32 +0700

    [guarantee-policy]-add test case edit

--- cypress/e2e/guarantee-policy/edit.cy.js
+++ cypress/e2e/guarantee-policy/edit.cy.js
@@ -1,6 +1,5 @@
 describe('template spec', () => {
 
-<<<<<<< HEAD
   it('กรอกข้อมูลแค่ Policies Name', () => {
     cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTgxMzI5NTMsImV4cCI6MTY5ODIxOTM1M30.EIzxIc5SeIBq2dysD-0qFO5C0LgjZFyyRDrL-70RwW0');
     cy.visit('https://shospitality.thesuperappcrm.com/main/guarantee-policies/e29f5a18-7ccc-415c-ae8a-a9d454585344')
@@ -176,52 +175,6 @@ describe('template spec', () => {
 
     cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(4)').click();
     cy.get('body > div:nth-child(4) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > div.ant-modal-confirm-btns > button.ant-btn.css-htwhyh.ant-btn-primary').click();
-=======
-  // before(() => {
-  //   cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTc3ODI2NjgsImV4cCI6MTY5Nzg2OTA2OH0.OZREenCHvakou4roaArJa5eeUE8OHb_mKC1AjyGHVB8');
-  // })
-  
-  // it("should not show slide menu on new form load", () => {
-  //   cy.visit("https://shospitality.thesuperappcrm.com/main/guarantee-policies/new");
-  //   cy.get(".slide-menu").should("not.be.visible");
-  // })
-
-  // it("should show Polices Name in slide menu on new form load", () => {
-  //   cy.visit("https://shospitality.thesuperappcrm.com/main/guarantee-policies/new");
-  //   cy.get(".slide-menu").should("contain", "Polices Name");
-  // })
-
-  it('create new', () => {
-    cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTc3ODI2NjgsImV4cCI6MTY5Nzg2OTA2OH0.OZREenCHvakou4roaArJa5eeUE8OHb_mKC1AjyGHVB8');
-    cy.visit('https://shospitality.thesuperappcrm.com/main/guarantee-policies/new')
-
-    cy.get('input[name="policy_name"]').type('Test2');
-    cy.get('input[name="policy_name"]').should('have.value', 'Test2');
-
-    cy.get('textarea[id="policy_desc"]').type('Test13');
-    cy.get('textarea[id="policy_desc"]').should('have.value', 'Test13');
-
-    cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
-    cy.contains('Record(s) have been successfully saved.');
-
-    // cy.visit('https://shospitality.thesuperappcrm.com/main/guarantee-policies')
-  })
-
-  it('create new1', () => {
-    cy.setCookie('hotel-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJjNDAxNDU4OS05MTRlLTRhOGUtYTgyYi1iYmQzYzMzNzFhZmIiLCJlbSI6ImFkbWluLXBvcnRhbEBlbWFpbC5jb20iLCJ1biI6ImFkbWluLXBvcnRhbCIsImZuIjoiQWRtaW4gSG9zcGl0YWxpdHkiLCJpYXQiOjE2OTc3ODI2NjgsImV4cCI6MTY5Nzg2OTA2OH0.OZREenCHvakou4roaArJa5eeUE8OHb_mKC1AjyGHVB8');
-    cy.visit('https://shospitality.thesuperappcrm.com/main/guarantee-policies/new')
-
-    cy.get('input[name="policy_name"]').type('Test00');
-    cy.get('input[name="policy_name"]').should('have.value', 'Test00');
-
-    cy.get('textarea[id="policy_desc"]').type('Test00');
-    cy.get('textarea[id="policy_desc"]').should('have.value', 'Test00');
-
-    cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
-    cy.contains('Record(s) have been successfully saved.');
-
-    // cy.visit('https://shospitality.thesuperappcrm.com/main/guarantee-policies')
->>>>>>> a27bf3c695cf85f68b476407a4c4b42e9c528f99
   })
 
 })
