8af0658030b6ed9fe14264bb579ad9b7ae4a8933
Thaksin <thakstho@METROSYSTEMS.CO.TH>
2023-11-16 13:58:14 +0700

    new sales startegy

--- cypress/e2e/cancellation-fee-policy/new.cy.js
+++ cypress/e2e/cancellation-fee-policy/new.cy.js
@@ -71,7 +71,7 @@ describe('template spec', () => {
       cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').type('1');
       cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').should('have.value', '1');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่  Policies Name เป็นภาษาไทย', () => {
@@ -81,10 +81,9 @@ describe('template spec', () => {
       cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/new')
   
       cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').type('ทดสอบ');
-      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').should('ทดสอบ');
+      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').should('have.value','ทดสอบ');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
-  
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
     })
     it('กรอกข้อมูลแค่  Policies Name เป็นอัครพิเเศษ */-+', () => {
 
@@ -93,9 +92,9 @@ describe('template spec', () => {
       cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/new')
   
       cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').type('+-*/');
-      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').should('+-*/');
+      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').should('have.value','+-*/');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่  Policies Name เป็นค่าว่าง ', () => {
@@ -104,10 +103,10 @@ describe('template spec', () => {
       cy.setCookie('hotel-token', token);
       cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/new')
   
-      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').type('');
-      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').should('');
+      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').type(' ');
+      cy.get('input[name="ms_cancellation_fee_policy,policy_name"]').should('have.value',' ');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
 
@@ -121,7 +120,7 @@ describe('template spec', () => {
       cy.get('.ant-select-item-option[title="USD - Dollar"]').click();
       cy.get('#rc-tabs-0-panel-0 > div > div.ant-col.ant-col-8.css-htwhyh > div > div > div > div > div.ant-collapse-content.ant-collapse-content-active > div > div > div.ant-col.ant-col-8.gutter-row.css-htwhyh > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div > div > span.ant-select-selection-item').should('have.text', 'USD - Dollar');
       
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Cancellation Fee Transaction Code', () => {
@@ -134,7 +133,7 @@ describe('template spec', () => {
       cy.get('.ant-select-item-option[title="Description"]').click();
       // cy.get('#rc-tabs-0-panel-0 > div > div.ant-col.ant-col-8.css-htwhyh > div > div > div > div > div.ant-collapse-content.ant-collapse-content-active > div > div > div.ant-col.ant-col-8.gutter-row.css-htwhyh > div > div > div.ant-col.ant-form-item-control.css-htwhyh > div > div > div > div > span.ant-select-selection-item').should('have.text', 'USD - Dollar');
       
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
 
@@ -147,7 +146,7 @@ describe('template spec', () => {
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').type('YourValueHere');
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').should('have.value', 'YourValueHere');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Policies Descriptione เป็นค่าว่าง', () => {
@@ -159,7 +158,7 @@ describe('template spec', () => {
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').type('.');
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').should('have.value', '.');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Policies Descriptione เป็นตัวเลข', () => {
@@ -171,7 +170,7 @@ describe('template spec', () => {
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').type('1');
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').should('have.value', '1');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Policies Descriptione เป็นภาษาไทย', () => {
@@ -183,7 +182,7 @@ describe('template spec', () => {
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').type('1');
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').should('have.value', '1');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Policies Descriptione เป็นอัครพิเเศษ', () => {
@@ -193,9 +192,9 @@ describe('template spec', () => {
       cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-fee-policies/new')
   
       cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').type('+-*/');
-      cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').should('+-*/');
+      cy.get('textarea[id="ms_cancellation_fee_policy_policy_desc"]').should('have.value','+-*/');
   
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Internal Loyalty Program Members', () => {
@@ -207,7 +206,7 @@ describe('template spec', () => {
       cy.get('#ms_cancellation_fee_policy_calculation_mapping_loyalty_member').click();
       // cy.get('body > div:nth-child(6) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-now > a').click();
 
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Other Guests', () => {
@@ -219,7 +218,7 @@ describe('template spec', () => {
       cy.get('#ms_cancellation_fee_policy_calculation_mapping_guest').type('12:00:00', { force: true });
       // cy.get('body > div:nth-child(7) > div > div > div > div > div.ant-picker-footer > ul > li.ant-picker-ok > button').click();
      
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
 
@@ -231,7 +230,7 @@ describe('template spec', () => {
   
       cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_to').type(10,{ force: true });
      
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Time Unit', () => {
@@ -243,7 +242,7 @@ describe('template spec', () => {
       cy.get('#ms_cancellation_fee_policy_calculation_mapping_time_unit').click();
       cy.get('.ant-select-item-option[title="Days"]').click();
      
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Calculation Methodt', () => {
@@ -255,7 +254,7 @@ describe('template spec', () => {
       cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_method').click();
       cy.get('.ant-select-item-option[title="Percent of Total Stay"]').click();
      
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
     it('กรอกข้อมูลแค่ Calculation Value', () => {
@@ -266,7 +265,7 @@ describe('template spec', () => {
   
       cy.get('#ms_cancellation_fee_policy_calculation_mapping_rule_calc_val').type(10);
      
-      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > a > button').click();
+      cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
   
     })
    
