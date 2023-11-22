886dde5ca9717806257ad2ea38c578f9ae074830
kittisak <kittiyim@METROSYSTEMS.CO.TH>
2023-11-22 09:06:34 +0700

    [new]add-auto test case new

--- cypress/e2e/cancellation-refund-policies/new.cy.js
+++ cypress/e2e/cancellation-refund-policies/new.cy.js
@@ -236,5 +236,31 @@ describe('template spec', () => {
 
   })
 
+  it('สร้าง Policies new', () => {
+    cy.viewport(1600, 1000);
+    cy.setCookie('hotel-token', token);
+    cy.visit('https://shospitality.thesuperappcrm.com/main/cancellation-refund-policies/new')
+
+    cy.get('input[name="ms_cancellation_refund_policy,policy_name"]').type('tt1');
+    cy.get('input[name="ms_cancellation_refund_policy,policy_name"]').should('have.value', 'tt1');
+
+    cy.get('#ms_cancellation_refund_policy_policy_currency').click();
+    cy.get('#ms_cancellation_refund_policy_policy_currency').type('AAA - aa', { force: true }).type('{enter}');
+
+    cy.get('textarea[id="ms_cancellation_refund_policy_policy_desc"]').type('ttt');
+    cy.get('textarea[id="ms_cancellation_refund_policy_policy_desc"]').should('have.value', 'ttt');
+
+    cy.get('#ms_cancellation_refund_policy_calculation_mapping_time_unit').click();
+    cy.get('#ms_cancellation_refund_policy_calculation_mapping_time_unit').type('Days', { force: true }).type('{enter}');
+
+    cy.get('#ms_cancellation_refund_policy_calculation_mapping_rule_calc_method').click();
+    cy.get('#ms_cancellation_refund_policy_calculation_mapping_rule_calc_method').type('Percent of Total Stay', { force: true }).type('{enter}');
+
+    cy.get('input[name="ms_cancellation_refund_policy_calculation_mapping,rule_calc_val"]').type('1.50');
+    cy.get('input[name="ms_cancellation_refund_policy_calculation_mapping,rule_calc_val"]').should('have.value', '1.50');
+
+    cy.get('#__next > section > main > div > div.tw-bg-blue-50 > div > div > button:nth-child(2)').click();
+
+  })
 
 })
